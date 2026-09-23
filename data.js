531bd0f1c1b5752e9e9217612cc5351dd115214a
SiQeR <41807296+SiQeR@users.noreply.github.com>
2026-09-23 17:57:12 +0300

    Опубликовать концертный песенник

---
+++ data.js
@@ -0,0 +1,791 @@
+window.PRESET_CHARTS = {
+  0: {source:'https://akkords.pro/songs/gorod-312/fonari/', chart:`{Вступление}
+[C#m] [B] [E] [G#]
+
+{Куплет}
+[C#m] [B] [E]
+[A] [F#m]
+[C#m] [B] [E]
+[A] [F#m]
+
+{Припев}
+[F#m] [A]
+[C#m] [B]
+[A] [F#m]
+
+{Следующий куплет}
+[C#m] [B] [E]
+[A] [F#m]
+!В некоторых разборах после F#m добавляют G#.`},
+  1: {source:'https://mychords.net/ru/trol/4390-mumij-troll-medvedica.html', chart:`{Вступление ×4}
+[Em] [C] [F#] [B]
+
+{Куплет}
+[Em] [C] [G] [B]
+[Em] [C] [G] [B]
+[Em] [Am] [G] [B]
+[C] [Am] [B]
+
+{Припев ×2}
+[Em] [C] [G] [B]
+
+{Повтор куплета и припева}
+
+{Концовка}
+[Em] [C] [F#] [B]
+[Em] [C] [F#] [B] [Em]`},
+  2: {source:'https://mychords.net/ru/nol/16277-nol-idu-kuryu.html', chart:`{Вступление}
+[E] [Am] | [E7] | [Am] [E] | [Am]
+[E] [Am] | [E7] | [Am] [D] | [Am]
+[E7] | [E7] | [Am] | [Am]
+
+{Куплет}
+[Am] [E7] | [Am] | [E7] | [Am]
+
+{Припев}
+[E7] [Am]
+[E7] [Am] [D] [Am]
+
+{Проигрыш}
+[Am] | [E7] | [E7] | [Am]
+
+!Повторяйте схему куплета и припева по форме вашей версии.`},
+  3: {source:'https://nagitaru.ru/zhenya-trofimov-poezda-akkordy/', chart:`{Оригинальная тональность: Cm}
+!В источнике приведён вариант Em с каподастром на 5-м ладу.
+
+{Вступление ×4}
+[D] [Em]
+
+{Куплет}
+[Em] [D] [C] [C/B]
+[Am] [C] [B7]
+
+{Предприпев}
+[C] [Am7] [C] [B7]
+
+{Припев ×2}
+[Em] [D] [C] [C/B]
+[Am] [C] [B7]
+
+{Проигрыш}
+[Em] [D] [C] [C/B] [Am] [C] [B7]`},
+  4: {source:'https://amdm.one/maks-korzh/zhit-v-kayf/', chart:`{Куплет 1 — начало}
+[Am] [C]
+[Am] [C]
+
+{Куплет 1 — развитие}
+[Am] [F] [C]
+[Am] [F] [C] [G]
+
+{Припев ×4}
+[Dm] [F] [Am] [G]
+
+{Куплет 2}
+[Am] [C]
+[Am] [F] [C] [G]
+
+{Припев ×4}
+[Dm] [F] [Am] [G]
+
+{Проигрыш ×3}
+[Am] [C]`},
+  5: {source:'https://mychords.net/ru/rjevskaya_m/36694-rzhevskaya-mariya-kogda-ya-stanu-koshkoj.html', chart:`{Вступление ×2}
+[Cm] | [Cm] | [Fm] | [G]
+
+{Куплет}
+[Cm] [Fm] [G]
+[Cm] [Fm] [G]
+
+{Предприпев}
+[Ab] [G] [Ab] [G]
+
+{Припев ×2}
+[Cm] [Cm7] [Cm] [Fm] [G]
+
+{Проигрыш}
+[Cm] | [Cm] | [Fm] | [G]
+
+{Повтор куплета и припева}`},
+  6: {source:'https://ru.muzland.ru/songs.html?auth=168&song=2', chart:`{Вступление}
+[Am] [Am/E] [Am/G#] [Am7/E]
+[Am7/G] [Am7/E] [D9/F#] [D9/E]
+[F] [F/C] [Am] [Am/E]
+[Fmaj] [Fmaj/C] [E7/D] [E7/C] [E7/B]
+
+{Куплет}
+[Am] [Am/G#] [Am7/G] [D7/F#]
+[F] [F/C] [Am]
+[Fmaj] [Fmaj/C] [E7/D] [E7/C] [E7/B]
+
+{Припев}
+[C] [G] [F] [F/C] [F] [G]
+[C] [G] [F] [F/C] [F] [E7] [E7/B]
+[F] [F/C] [Am]
+[F] [F/C] [A] [A/C#] [A/D] [A/E]
+[Dm] [Bm7] [E] [Am]
+
+{Проигрыш и кода}
+Повтор вступления.
+!Подробная гармония близка к записи; при необходимости упрощайте басовые ноты.`},
+  7: {source:'https://ru.muzland.ru/songs.html?auth=7&song=24', chart:`{Вступление ×8}
+[C#m7] [C#m7/G#]
+
+{Куплет}
+[C#m7]
+
+{Проигрыш}
+[C#m7] [C#m7/B] [C#m7/A#]
+
+{Припев}
+[A] [Aadd9] [A]
+[A] [A/B] [Asus4/C] [C#m7]
+[C#m7] [C#m7/B] [C#m7/A#]
+[A] [Aadd9] [A]
+[A] [A/B] [Asus4/C] [C#m7]
+
+{Проигрыш ×4}
+[C#m7] [C#m7/G#]
+
+!Это подробный вариант, близкий к оригинальной записи.`},
+  8: {source:'https://www.5lad.net/akkordy/valeriya/samoljot', chart:`{Куплет}
+[Bm] [G] [Em7] [F#7] ×4
+
+{Припев}
+[Em7] [Bm7] ×2
+[G] [Em] [F#7] ×2
+
+{Куплет}
+[Bm] [G] [Em7] [F#7] ×4
+
+{Припев ×2}`},
+  9: {source:'https://mychords.net/uk/mihailov/72121-stas-mixajlov-zolotoe-serdce.html', chart:`{Золотое сердце — песня со строкой «сердце из чистого золота»}
+{Куплет}
+[Cm] [Bb] [Eb] [Fm] [G] [Cm]
+[Bbm] [C] [Fm] [Dm7b5] [Db] [G]
+
+{Припев ×2}
+[Cm] [Fm] [Bb] [Eb]
+[Cm] [Fm] [G] [Cm]
+
+{Повтор куплета и припева}`},
+  11: {source:'https://ru.muzland.ru/songs.html?auth=79&song=1', chart:`{Вступление}
+[Fm] | [Cm] | [Db] | [Ab] [Eb/G]
+[Fm] | [Cm] | [Bbm] | [Db] [Eb]
+[Fm] [Fm] [Fm] [Fmadd9] ×2
+
+{Куплет}
+[Fm] [Db] [Bbm] [Fm] [Fm7]
+[Fm] [Db] [Bbm] [C7]
+
+!Подробный подбор Muzland; последующие разделы откройте по ссылке источника.`},
+  12: {source:'https://akkords.pro/songs/lumen/sid-i-nensi/', chart:`{Куплет}
+[Dm] [C] [Bb] [C] [A] [Dm]
+
+{Рефрен}
+[Dm] [C] [Bb] [C] [A] [Dm]
+
+{Повтор куплета и рефрена}
+[Dm] [C] [Bb] [C] [A] [Dm]
+
+{Проигрыш}
+[Dm] [C] [Bb] [C] [A] [Dm]
+
+!В этом подборе схема повторяется на протяжении песни.`},
+  13: {source:'https://mychords.net/ru/animal-dzhaz/259867-animal-dzhaz-tri-poloski.html', chart:`{Вступление ×2}
+[Dm] [Dm] [Bb] [F]
+
+{Куплет}
+[Dm] [Dm] [Bb] [F] ×4
+
+{Припев ×2}
+[F] [Dm] [Am] [Bb]
+
+{Проигрыш ×2}
+[Dm] [Dm] [F] [Dm] [C]
+
+{Куплет, припев}
+
+{Бридж ×4}
+[Am] [Bb] [F] [Dm] [C]
+
+{Финальный припев}
+[F] [Dm] [Am] [Bb]`},
+  14: {source:'https://akkordam.ru/sergej-lazarev-v-samoe-serdce/', chart:`{Куплет}
+[Em] [G] [Am] [Em]
+[Em] [G] [Am] [B7]
+
+{Предприпев}
+[C] [D] [Em]
+[G] [C] [B7]
+
+{Припев}
+[B7] [Em] [G] [C] [B7]
+[Em] [G] [C] [B7]
+[Em] [B7] [C] [B7]
+
+{Повтор куплета и припева}`},
+  15: {source:'https://mychords.net/ru/pilot/8253-pilot-shnurok.html', chart:`{Куплет}
+[Em] [B7] [Em] [B7]
+[Em] [B7] [Em] [B7] [C]
+[B7] [C] [B7]
+
+{Припев ×2}
+[G] [Am]
+[F] [B7] [Em] [C] [Am] [B7]
+
+{Проигрыш ×2}
+[Em] [B7] | [Em] [Am] | [C] | [B7]
+
+{Повтор куплета и припева}
+
+{Кода ×8}
+[Em] [B7] | [Em] [Am] | [C] | [B7]`},
+  16: {source:'', chart:`{Куплет 1}
+[Em]Я призраком в ночи
+[Em]Необитованных врат
+[Am]Предсказал свою судьбу,
+[Bm]что больше нет пути назад
+[Em]Моя бездна не скулит,
+[C]в ней давно уж полегли
+[Am]Чем-то большим стать [Bm]надежды
+
+{Припев}
+[Em]Зажечь огни Земли
+[C]Разжечь их по сторонам
+[Am]Пока наш огонь горит
+[Bm]Нестрашно идти туда
+[Em]Где мраморны облака
+[C]Где в грудь проникает смех
+[Am]И на вопрос, как дела
+[Bm]Отвечу, что лучше всех
+[Em]Зажечь огни Земли
+[C]Зажечь их по городам
+[Am]Ударит Царство Теней
+[Bm]Я обернусь Фениксом
+[Em]Ликуй, царица ночь
+[C]Пускай страсть по сердцам
+[Am]И если уйду во тьму
+[Bm]Что делать, я знаю сам
+Всё знаю сам
+
+{Постприпев}
+[Em]Знаю сам (Ууу)
+[C]Знаю сам (Ууу)
+[Am]Знаю сам, знаю сам
+[Bm]Знаю сам
+[Em]Знаю сам (Ууу)
+[C]Я знаю сам (Ууу)
+[Am]Всё знаю сам, знаю сам
+[Bm]Знаю сам
+
+{Куплет 2}
+[Em]Чтобы наверх путь выломать
+[Em]Нужно себя создать
+[Am]Крути шестеренки и [Bm]вырывать с корнями
+[Em]Что тянет рыдать
+[C]Новою волной наполняю реки
+[Am]Соберу лучшее, что есть в человеке
+[Bm]Героем может стать каждый
+
+{Припев}
+[Em]Зажечь огни Земли
+[C]Разжечь их по сторонам
+[Am]Пока наш огонь горит
+[Bm]Нестрашно идти туда
+[Em]Где мраморны облака
+[C]Где в грудь проникает смех
+[Am]И на вопрос, как дела
+[Bm]Отвечу, что лучше всех
+[Em]Зажечь огни Земли
+[C]Зажечь их по городам
+[Am]Ударит Царство Теней
+[Bm]Я обернусь Фениксом
+[Em]Ликуй, царица ночь
+[C]Пускай страсть по сердцам
+[Am]И если уйду во тьму
+[Bm]Что делать, я знаю сам
+Всё знаю сам
+
+{Постприпев}
+[Em]Знаю сам (Ууу)
+[C]Знаю сам (Ууу)
+[Am]Знаю сам, знаю сам
+[Bm]Знаю сам
+[Em]Знаю сам (Ууу)
+[C]Я знаю сам (Ууу)
+[Am]Всё знаю сам, знаю сам
+[Bm]Знаю сам
+
+{Финал}
+[Em]Зажечь огни Земли
+[C]Разжечь их по сторонам
+[Am]Пока наш огонь горит
+[Bm]Нестрашно идти туда
+[Em]Где мраморны облака
+[C]Где в грудь проникает смех
+[Am]И на вопрос, как дела
+[Bm]Отвечу, что лучше всех`},
+  17: {source:'https://ru.muzland.ru/songs.html?auth=162&song=5', chart:`{Вступление}
+[Am]
+
+{Куплет}
+[Am] [C] [Fmaj] [Dm] [G]
+[Am] [Fmaj] [C] [Fmaj] [Dm] [G]
+
+{Припев ×2}
+[Am] [Dm] [G] [C] [E7]
+[Am] [Dm] [E7]
+
+{Проигрыш ×2}
+[Am] | [Dm] | [G] | [C] [E7]
+
+{Куплет и припев ×2}`},
+  18: {source:'https://ru.muzland.ru/songs.html?auth=90&song=2', chart:`{Вступление}
+[Am]
+
+{Куплет}
+[Am] [F/A] [C/G] [G]
+[Am] [F] [C] [G]
+[Am] [F] [C] [G/B] [Am]
+[F] [C] [G]
+
+{Припев ×2}
+[Am] [F] [C] [G]
+[Am] [F] [C] [G] [Am]
+
+{Куплет и припев ×2}`},
+  20: {source:'https://mychords.net/ru/akula/264493-akula-takaya-lyubov.html', chart:`{Куплет}
+[Bm] [Em] [G] [F#] ×3
+[Bm] [Em] [G] [F#] [G]
+
+{Припев ×4}
+[Bm] [G] [D] [F#]
+
+{Проигрыш ×2}
+[Bm] | [G] | [D] | [F#]
+
+{Куплет, припев, проигрыш}
+
+{Бридж}
+[Bm] [Em] [G] [F#]
+[Bm] [Em] [G] [F#] [G]
+
+{Финальный припев}`},
+  21: {source:'https://www.5lad.net/akkordy/via-gra/ya-ne-vernus', chart:`{Куплет}
+[Am] [G] [F] [Dm] [Am]
+
+{Припев}
+[Am] [G] [G] [C]
+[Am] [G] [G] [C] [E]
+
+{Куплет и припев ×2}`},
+  22: {source:'https://mychords.net/ru/meladze/206513-meladze-valerij-salyut-vera.html', chart:`{Куплет}
+[D] [G] [A7] [D]
+[G] [A7] [Bm]
+[D] [G] [A7] [D]
+[G] [A7] [F#]
+
+{Предприпев}
+[Bm] [G] [Em] [F#]
+
+{Припев}
+[Bm] [Em] [A]
+[D] [G] [Em] [F#]
+[Bm] [Em] [A]
+[D] [G] [Em] [F#]
+
+{Повтор куплета и припева}`},
+  23: {source:'https://amdm.ru/akkordi/svintsovyy_tuman/117309/ya_znay_pridet_tot_den/', chart:`{Куплет}
+[Am] [F] [C] [G]
+[Am] [F] [C] [F]
+[Am] [F] [C] [G]
+
+{Предприпев}
+[Am] [Am7] [F7] [Dm] [F] [G]
+
+{Припев ×2}
+[Am] [F] [C] [G]
+
+{Повтор куплета и припева}`},
+  24: {source:'https://mychords.net/ru/nasyrov_m/25057-m-nasyrov-ya-yeto-ty.html', chart:`{Куплет}
+[F#m] [Bm] [E] [A] [D]
+[B] [E] [C#]
+[F#m] [Bm] [E] [A] [D]
+[B] [E] [C#]
+
+{Припев ×2}
+[F#m] [Bm] [E] [C#] [F#m]
+
+{Куплет и припев ×2}`},
+  25: {source:'https://amdm.ru/akkordi/yuriy_titov/89391/ponaroshku/', chart:`{Вступление}
+[Em] [Am] [B] ×2
+
+{Куплет}
+[Em] [Am] [B] ×2
+[Em] [C] [Am] [B] [C] [B]
+
+{Припев ×2}
+[Em] [Am] [D] [G]
+[C] [Am] [B]
+
+{Повтор куплета и припева}`},
+  19: {source:'https://mytabs.ru/akkordy/r-r/ruki-vverh/nazovi-ego-kak-menya_14011.html', chart:`{Куплет}
+[Em] [C] [Am] [D] ×4
+
+{Припев}
+[Em] [C] [D] [C] [D]
+[Em] [C] [Am] [D]
+[Em] [C] [D] [G]
+[Em] [C] [D] [G] [D] [Em]
+
+{Куплет и припев}`},
+  26: {source:'https://amdm.ru/akkordi/evgeniya_otradnaya/102568/uhodi_i_dver_zakroy/', chart:`{Куплет}
+[Em] [B7] [G] [C]
+[Am] [Em] [Am] [B7] [Em]
+[Em] [B7] [G] [D]
+[C] [Em] [Am] [B7]
+
+{Припев ×2}
+[Em] [Am] [D] [G]
+[Em] [Am] [B7] [Em]
+
+{Повтор куплета и припева}`},
+  27: {source:'https://akkords.pro/songs/lyapis-trubetskoy/kapital/', chart:`{Вступление ×6}
+[Am] [F] [Dm] [E]
+
+{Куплет}
+[Am] [F] [Dm] [E]
+[Am] [F] [D] [E]
+
+{Припев}
+[Am] [F] [Dm] [E]
+[Am] [F] [D] [E]
+[Am] [F] [Dm] [E] ×4
+
+{Повтор куплета и припева}
+
+{Проигрыш ×4}
+[Am] [F] [Dm] [E]`},
+  28: {source:'https://mychords.net/ru/sabi/257897-mia-boyka-sabi-bazovyy-minimum.html', chart:`{Вступление ×2}
+[F#m] [G#m] [C#m]
+
+{Куплет}
+[F#m] [G#m] [C#m] ×4
+
+{Предприпев}
+[F#m] [G#m] [C#m] ×2
+
+{Припев}
+[F#m] [G#m] [C#m] ×4
+
+{Куплет, предприпев, припев}
+!В этой аранжировке последовательность сохраняется почти всю песню.`},
+  29: {source:'https://mychords.net/ru/pornofilmy/129225-pornofilmy-tak-soskuchilsya.html', chart:`{Вступление ×4}
+[Am] [Fmaj7] [Dm] [E]
+
+{Куплет}
+[Am] [G] [C]
+[Am] [Dm] [F] [Bb] [E]
+[Am] [Bsus] [Bmadd11] [Dm] [E7] [Am] [F] [E7]
+[Am] [Bsus] [Bmadd11] [Dm] [F] [E] [Esus] [E]
+
+{Припев}
+[Am] [Fadd9] [C] [E7] ×4
+[Am] [Fmaj7] [Dm] [E] ×4
+
+{Куплет и припев}
+
+{Соло — модуляция на +2}
+[Bm] [Gadd9] [Em] [F#] ×4
+
+{Финальный припев}
+[Bm] [Gadd9] [D] [F#] ×4
+[Bm] [G] [Em] [F#] ×4`},
+  30: {source:'https://guitarfightclub.ru/chords/leonid-agutin-na-sirenevoy-lune/', chart:`{Куплет}
+[G] [D] [Em] [D] [C]
+[D] [Am] [D] [G]
+
+{Предприпев}
+[G] [D] [C] [Am] [Em] [Am] [C] [D] ×2
+
+{Припев ×2}
+[G] [D] [Am] [Em] [D]
+
+{Повтор куплета и припева}`},
+  31: {source:'https://akkords.pro/songs/nochnye-snaypery/31-ya-vesna/', chart:`{Куплет}
+[Em] [C] [Am] [Em] [B7] ×2
+
+{Припев}
+[C] [Am] [G] [B7]
+[C] [Am] [G] [B7]
+
+{Куплет и припев}
+
+{Проигрыш}
+
+{Финальный припев}`},
+  32: {source:'https://ru.muzland.ru/songs.html?auth=88&song=13', chart:`{Вступление}
+[Gm] | [Gmadd9] | [Cm] | [Cm7]
+[Eb] | [Cm6] | [D] | [D]
+
+{Куплет ×2}
+[Gm] [Eb] [Cm7] [D7]
+
+{Припев}
+[Gm] [Gm+] [Gm] [Eb]
+[Gm] [Gm+] [Gm] [Fsus4] [F]
+
+{Куплет и припев}
+
+{Проигрыш}
+[Eb] [Ebadd11+] [F] [Fadd11] [D]
+[Eb] [Ebadd11+] [F]
+[Gm] [Gmadd9] [Eb]
+[Cm7] [Cm6] [D]
+
+{Припев и кода}`},
+  33: {source:'https://www.5lad.net/akkordy/nautilus-pompilius/krylya', chart:`{Вступление ×2}
+[Em] [B7] [Am] [B7]
+
+{Куплет}
+[Em] [B7] [Am] [B7] ×2
+
+{Припев}
+[F] [Em] [F] [Em]
+[D] [Em] [D] [Em]
+
+{Куплет и припев ×2}`},
+  34: {source:'https://akkords.pro/songs/korol-i-shut/lesnik/', chart:`{Вступление ×4}
+[Am] [F] [Dm] [G]
+
+{Куплет}
+[Am] [F] [G] ×3
+[Am] [D5] [E5] [G5]
+
+{Припев ×2}
+[Am] [F] [Dm] [G]
+
+{Проигрыш ×2}
+[A5] [G5] [F5] [C5]
+
+{Куплет и припев}
+
+{Куплет 3 и припев}
+!В оригинале третий куплет переходит в Bm; в этом акустическом подборе оставлен Am.`},
+  35: {source:'https://akkords.pro/songs/korol-i-shut/prokljatyj-staryj-dom/', chart:`{Вступление ×2}
+[Bm] [A6] [Emadd9] [Em] [Gadd9] [G] [Gmaj7]
+
+{Куплет}
+[Bm] [A] [Em] [F#m] ×2
+[G] [A] [F#m] [Bm]
+[G] [A] [F#m] [Bm] [A] [G]
+
+{Припев ×4}
+[Bm] [G] [A] [F#m]
+
+{Проигрыш ×2}
+[G] [A] [D] [Bm]
+
+{Повтор куплета и припева}
+
+{Бридж ×4}
+[F5] [F#5] [A5] [G5]
+
+{Кода}
+[Bm]`},
+  36: {source:'https://akkords.pro/songs/korol-i-shut/kukla-kolduna/', chart:`{Вступление ×4}
+[Dm] [Gm] [A]
+
+{Куплет}
+[Dm] [F] [Gm] [A] ×4
+[Gm] [A] [C]
+
+{Припев ×2}
+[Dm] [Gm] [A]
+
+{Проигрыш ×2}
+[Dm] [Gm] [A]
+
+{Повтор куплета и припева}
+
+{Бридж}
+[Bb] [C] [F] [Bb] [Gm] [A]
+
+{Проигрыш ×4}
+[Dm] [Gm] [A]`},
+  37: {source:'https://akkords.pro/songs/korol-i-shut/prygnu-so-skaly/', chart:`{Куплет}
+[Dm] [Gm7] [Am7] ×2
+[F] [Gm] [Bb] [Am7]
+[F] [Gm] [A]
+
+{Припев}
+[Dm] [C] [Gm] [A] ×2
+[F] [Am] [Gm] [A] [Dm]
+
+{Проигрыш ×2}
+[Dm] [C] [Gm] [A]
+
+{Повтор куплета и припева}`},
+  38: {source:'https://akkords.pro/songs/korol-i-shut/vospominaniya-o-byloy-lyubvi/', chart:`{Каподастр: 3 лад в подборе}
+{Вступление ×2}
+[Em] [Am] [D] [G] [C] [Am] [B7]
+
+{Куплет}
+[Em] [Am] [D] [G]
+[C] [Am] [B7] [Em]
+[Em] [Am] [D] [G]
+[C] [Am] [B7]
+
+{Припев}
+[Em] [Am] [D] [G] [C]
+[Am] [D] [G]
+[C] [Am]
+[Em] [Am] [D] [G] [C]
+[Am] [D] [G]
+[C] [Am] [B7]
+
+{Повтор куплета и припева}`},
+  39: {source:'https://guitarfightclub.ru/chords/mumiy-troll-malysh/', chart:`{Куплет ×4}
+[Em] [Bm] [C] [D]
+
+{Припев}
+[C] [D] [Bm] [Em] ×3
+[D] [G] [Em] [C] [D]
+
+{Повтор куплета и припева}`},
+  40: {source:'https://akkords.pro/songs/kino/gruppa-krovi/', chart:`{Куплет}
+[F#m] [C#m] [F#m] [E]
+[F#m] [C#m] [F#m] [E]
+
+{Припев}
+[F#m] [C#m] [Bm] [E]
+[F#m] [C#m] [Bm] [E] [F#m]
+
+{Проигрыш}
+!Гитарный рифф и соло приведены в источнике.
+
+{Куплет и припев}`},
+  41: {source:'https://akkords.pro/songs/kino/konchitsja-leto/', chart:`{Вступление ×2}
+[Em] [D] [Bm] [C] [Am] [D]
+
+{Куплет}
+[Em] [Am] [Em] ×2
+
+{Припев}
+[D] [C] [Em] ×4
+
+{Проигрыш ×2}
+[Em] [D] [Bm] [C] [Am] [D]
+
+{Куплет, припев, проигрыш}
+
+{Финальный припев ×2}
+[D] [C] [Em] ×4
+
+{Кода ×6}
+[Em] [D] [Bm] [C] [Am] [D]`},
+  42: {source:'https://akkords.pro/songs/kino/spokojnaya-noch/', chart:`{Вступление ×2}
+[Am] [G] [Dm] [C]
+
+{Куплет}
+[Am] [G] [Dm] [Am] ×2
+
+{Припев ×2}
+[F] [Em] [Am]
+[F] [C] [Am]
+
+{Проигрыш ×2}
+[Am] [G] [Dm] [C]
+
+{Куплет и припев ×2}
+
+{Соло ×4}
+[F] [Em] [Am] [Am] [F] [C] [Am] [Am]`},
+  43: {source:'https://akkords.pro/songs/mumiy-troll/vladivostok-2000/', chart:`{Вступление}
+[Am] [Dm7] [C] [E7] ×2
+
+{Куплет 1}
+[Am] ×2
+[Dm] ×2
+[C] [E]
+
+{Припев}
+[Am] [Dm7] [C] [E7] ×4
+
+{Куплет 2, припев}
+[Am] [Dm] [C] [E]
+[Am] [Dm7] [C] [E7] ×4`},
+  44: {source:'https://akkords.pro/songs/agata-kristi/kak-na-vojne/', chart:`{Вступление ×8}
+[Am] [G] [C] [E]
+
+{Куплет}
+[Am] [G] [C] [E] ×4
+
+{Припев}
+[Am] [G] [C] [E] ×4
+
+{Проигрыш, второй куплет и припев}
+[Am] [G] [C] [E]`},
+  45: {source:'https://akkords.pro/songs/splin/vyxoda-net/', chart:`{Куплет 1}
+[C] [G] [D] [Em]
+[C] [G]
+[C] [G] [D]
+
+{Припев}
+[Em] [C] [G] [D] ×2
+[C] [D] [Em]
+
+{Куплет 2, припев}
+[C] [G] [D] [Em]
+[Em] [C] [G] [D] ×2
+[C] [D] [Em]`},
+  46: {source:'https://akkords.pro/songs/chizh/18-beryoz/', chart:`{Куплет}
+[Am] [Dm] [E] ×4
+
+{Припев}
+[Am] [Dm] [G] [C]
+[Am] [D] [B7] [E]
+
+{Куплет и припев}
+[Am] [Dm] [E] ×4
+[Am] [Dm] [G] [C]
+[Am] [D] [B7] [E]
+
+{Проигрыш}
+[Dm] [B7] [E] [Am] [C]
+[Dm] [B7] [E]`},
+  47: {source:'https://akkords.pro/songs/ddt/eto-vsyo/', chart:`{Вступление}
+[G] [D] [Em] [C] [D]
+
+{Куплет 1}
+[G] [D/F#] [Em]
+[C] [G] [D]
+[G] [D/F#] [Em]
+[C] [D]
+
+{Припев ×2}
+[G] [D/F#] [Em] [C]
+[G] [D] [G]
+
+{Куплет 2 и припев}
+[G] [D/F#] [Em]
+[C] [D]
+[G] [D/F#] [Em] [C]
+[G] [D] [G]`},
+  10: {source:'https://www.youtube.com/watch?v=KaHLpGbgL9s', chart:`{Куплет}
+[Em] [Am]
+[D] [G] [C]
+[Am] [B] [B7]
+
+[Em] [Am]
+[D] [G] [C]
+[Am] [B] [B7]
+
+{Припев}
+[Em] [Am]
+[D] [G]
+[C] [Am]
+[B] [B7]
+
+{Повтор куплета и припева}`}
+};
